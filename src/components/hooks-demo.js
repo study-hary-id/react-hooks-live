64aaf1243213650ddf44514a2f0b1fce43540d2e
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-09 17:38:15 +0700

    Implement react hooks useState

---
+++ src/components/hooks-demo.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function HooksDemo(props) {
+  const [name, setName] = useState("Agata");
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <section>
+      <form autoComplete="off">
+        <section>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </section>
+      </form>
+
+      <p>Hello, {name}!</p>
+    </section>
+  );
+}
