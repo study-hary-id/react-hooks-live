1dfa08923e0e09ce22021a8f76ed6ee3dd804490
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-10 09:40:25 +0700

    Import Navbar and use BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar";
+import ClassDemo from "./components/class-demo";
+import HooksDemo from "./components/hooks-demo";
 
 function App() {
+  const links = [
+    { name: "Class", path: "/" },
+    { name: "Hooks", path: "/hooks-demo" }
+  ];
+
   return (
-    <div>
-      Hello, World!
-    </div>
+    <Router>
+      <header>
+        <Navbar links={links} />
+      </header>
+      <main>
+        Hello, World!
+      </main>
+    </Router>
   );
 }
 
