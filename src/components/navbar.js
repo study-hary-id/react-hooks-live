b594684ce2f0f63ce17f70a67b77380c33496085
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-14 15:51:52 +0700

    Replace Link into NavLink

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,16 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar({ links }) {
   const navLinks = links.map((link, key) => (
     <li
       key={key}
       className="mr-6 font-semibold text-blue-900 hover:text-blue-600">
-      <Link to={link.path}>{link.name}</Link>
+      <NavLink
+        to={link.path}
+        // Add condition because activeClassName doesn't work.
+        className={({ isActive }) => (isActive ? "text-blue-600" : "")}
+      >{link.name}</NavLink>
     </li>
   ));
 
